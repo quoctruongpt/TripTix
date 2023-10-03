20d241b6bb945dc9c024228f3becd41324afa872
Quoc Truong <dinhquoctruong97@gmail.com>
2023-10-03 23:01:02 +0700

    react hook form

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,46 +1,91 @@
 import React from "react";
 import { Text, Input, Button } from "@rneui/themed";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet, View } from "react-native";
+import {
+  Keyboard,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { Controller, useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+const schema = yup.object().shape({
+  email: yup.string().required("require").min(4, "min 4"),
+  password: yup.string().required("require").min(4, "min 4"),
+});
 
 export const SignIn = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    defaultValues: { email: "", password: "" },
+    resolver: yupResolver(schema),
+  });
+
+  const handleSignIn = handleSubmit((data) => {
+    console.warn(data);
+  });
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <Text h4 h4Style={styles.title}>
-          Login Account
-        </Text>
-        <Text>Hello, welcome back to our account</Text>
-      </View>
-      <View>
-        <Input
-          errorMessage="Oops! that's not correct."
-          inputStyle={styles.inputStyle}
-          leftIcon={<Icon name="email-lock" size={20} />}
-          placeholder="Email ID"
-        />
-        <Input
-          errorMessage="Oops! that's not correct."
-          inputStyle={styles.inputStyle}
-          leftIcon={<Icon name="shield-key" size={20} />}
-          placeholder="Password"
-        />
-        <Button
-          title="Login"
-          containerStyle={styles.btnLoginContainer}
-          buttonStyle={styles.btnLogin}
-        />
-      </View>
-      <View style={styles.footer}>
-        <Text>Not Registered yet?</Text>
-        <Button
-          title="Create an Account"
-          type="clear"
-          titleStyle={styles.btnSignUp}
-        />
-      </View>
-    </SafeAreaView>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text h4 h4Style={styles.title}>
+            Login Account
+          </Text>
+          <Text>Hello, welcome back to our account</Text>
+        </View>
+        <View>
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                errorMessage={errors.email?.message as string}
+                inputStyle={styles.inputStyle}
+                leftIcon={<Icon name="email-lock" size={20} />}
+                placeholder="Email ID"
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                errorMessage={errors.password?.message as string}
+                inputStyle={styles.inputStyle}
+                leftIcon={<Icon name="shield-key" size={20} />}
+                placeholder="Password"
+                value={value}
+                onChangeText={onChange}
+              />
+            )}
+          />
+          <Button
+            title="Login"
+            containerStyle={styles.btnLoginContainer}
+            buttonStyle={styles.btnLogin}
+            onPress={handleSignIn}
+          />
+        </View>
+        <View style={styles.footer}>
+          <Text>Not Registered yet?</Text>
+          <Button
+            title="Create an Account"
+            type="clear"
+            titleStyle={styles.btnSignUp}
+          />
+        </View>
+      </SafeAreaView>
+    </TouchableWithoutFeedback>
   );
 };
 
