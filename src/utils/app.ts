0a3082db8db221412d70df91cdda017b56edba93
Quoc Truong <dinhquoctruong97@gmail.com>
2023-11-25 23:36:16 +0700

    update

--- src/utils/app.ts
+++ src/utils/app.ts
@@ -28,11 +28,8 @@ export async function registerForPushNotificationsAsync() {
         alert("Failed to get push token for push notification!");
         return;
       }
-      token = (
-        await Notifications.getExpoPushTokenAsync({
-          projectId: Constants.expoConfig.extra.eas.projectId,
-        })
-      ).data;
+      token = (await Notifications.getDevicePushTokenAsync()).data;
+      console.log(token);
     } else {
       alert("Must use physical device for Push Notifications");
     }
